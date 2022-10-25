b8b05563b2c2387ec86fb680224e95e4d4d4b4d3
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-25 23:29:32 +0530

    errors

---
+++ 03-errors/program.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var DivideByZeroError = errors.New("divisor cannot be 0")
+
+func main() {
+	/*
+		quotient, remainder, err := divide(100, 0)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Dividing 100 by 7, quotient = %d and remainder = %d\n", quotient, remainder)
+	*/
+
+	quotient, remainder, err := divide(100, 0)
+	if err == DivideByZeroError {
+		fmt.Println("do not attempt to divide by zero")
+		return
+	}
+	if err != nil {
+		fmt.Println("something went wrong")
+		return
+	}
+	fmt.Printf("Dividing 100 by 7, quotient = %d and remainder = %d\n", quotient, remainder)
+}
+
+/*
+func divide(x, y int) (int, int, error) {
+	if y == 0 {
+		err := errors.New("divisor cannot be 0")
+		return 0, 0, err
+	}
+	quotient := x / y
+	remainder := x % y
+	return quotient, remainder, nil
+}
+*/
+
+/*
+func divide(x, y int) (quotient, remainder int, err error) {
+	if y == 0 {
+		err = errors.New("divisor cannot be 0")
+		return
+	}
+	quotient = x / y
+	remainder = x % y
+	return
+}
+*/
+
+func divide(x, y int) (quotient, remainder int, err error) {
+	if y == 0 {
+		err = DivideByZeroError
+		return
+	}
+	quotient = x / y
+	remainder = x % y
+	return
+}
