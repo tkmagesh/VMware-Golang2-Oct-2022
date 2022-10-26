3bf75c4c74b70b124df988ebbeb234ad347e1a84
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-27 00:14:11 +0530

    buffered channels

---
+++ 12-concurrency/22-demo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int, 2) // buffered channel
+	fmt.Println("len(ch) = ", len(ch))
+	ch <- 100
+	ch <- 200
+	fmt.Println("len(ch) = ", len(ch))
+	/*
+		fmt.Println(<-ch)
+		fmt.Println(<-ch)
+	*/
+}
